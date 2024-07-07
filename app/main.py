525a6f45be81b3bf6d12210d15c0356227e2af23
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-07-07 21:30:15 +0300

    create function "format_single_linter_file()"

--- app/main.py
+++ app/main.py
@@ -9,8 +9,14 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [
+            format_linter_error(error)
+            for error in errors
+        ],
+        "path": file_path,
+        "status": ("failed" if errors else "passed")
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
